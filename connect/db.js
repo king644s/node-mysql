6052b0e4d5de5aa1a9ab4c73c1c596aecb411c0f
king644s <sonivatsal3@gmail.com>
2022-11-08 00:49:07 +0530

    postgress db connected

--- connect/db.js
+++ connect/db.js
@@ -1,10 +1,32 @@
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("test", "root", "", {
-  host: "localhost",
-  dialect:
-    "mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
-});
+// for online heroku postgress
+const sequelize = new Sequelize(
+  "d7arrmnr91fro8",
+  "cvawvhyjljxkit",
+  "952deeadbe50ce542aa661f1202ea1cfb5ea20bf935d97e20b961de218f11fd9",
+  {
+    host: "ec2-3-227-68-43.compute-1.amazonaws.com",
+    port: 5432,
+    dialect:
+      "postgres" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false, // This line will fix new error
+      },
+    },
+  }
+);
+// const sequelize = new Sequelize(process.env.DATABASE_URL); // Example for postgres
+
+//for local
+// const sequelize = new Sequelize("test", "root", "", {
+//   host: "localhost",
+//   dialect:
+//     "mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+// });
 
 sequelize
   .authenticate()
@@ -15,5 +37,4 @@ sequelize
     console.error("Unable to connect to the database:", err);
   });
 
-
-module.exports = sequelize
+module.exports = sequelize;
